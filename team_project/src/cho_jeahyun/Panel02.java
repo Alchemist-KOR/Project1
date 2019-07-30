6257762c08e10981e12676fb158e43d9d99d215a
ddung <ddung@MSI-GS73VR>
2019-07-30 16:26:29 +0900

    패널2 패널 7 수정

--- team_project/src/cho_jeahyun/Panel02.java
+++ team_project/src/cho_jeahyun/Panel02.java
@@ -1,17 +1,24 @@
 package cho_jeahyun;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Panel02 extends JPanel{
+public class Panel02 extends JPanel implements ActionListener{
+	
+
+
 	private JButton login;
 	private JPanel north,north_east, center,center_content;
 	private JPanel content_1,content_2,content_3;
@@ -24,16 +31,87 @@ public class Panel02 extends JPanel{
 	private Image resizeimage;
 	private Image result;
 
+	private JComboBox room_select;
 	
+	private String room[] = {"선택하세요.","스텐다드", "프리미엄", "스위트"};
 	
-	
-	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object combo = e.getSource();
+		if(room_select.getSelectedIndex() == 1) {
+			center_content.removeAll();
+			rating1.setText("스텐다드");
+			img1 = new ImageIcon("img//호텔02(스텐다드).png");
+			resizeimage = img1.getImage();
+			result = resizeimage.getScaledInstance(550, 350, Image.SCALE_SMOOTH);
+			res_img1 = new ImageIcon(result);
+			imglabel1 = new JLabel(res_img1);
+			rating1.setBounds(65,7,55 , 15);
+			text1.setBounds(65, 340, 420, 120);
+			imglabel1.setBounds(65, 25, 550, 350);
+
+			text1.setText("<html><body>기본타입 룸<br>스튜디오 타입의 파스텔 톤 객실에 휴식을 취할 수 있는"
+				+ "테이블과 체어, <br>그리고 라탄 타입의 아늑한 체어를 완비한 OO의 가장 기본적인 룸입니다.</body></html>");
+			center_content.add(rating1);
+			center_content.add(imglabel1);
+			center_content.add(text1);
+			center_content.add(room_select);
+			center_content.repaint();
+		}else if(room_select.getSelectedIndex() == 2) {
+			center_content.removeAll();
+			rating1.setText("프리미엄");
+			imglabel1 = new JLabel(res_img1);
+			img1 = new ImageIcon("img//호텔03(프리미엄).png");
+			resizeimage = img1.getImage();
+			result = resizeimage.getScaledInstance(550, 350, Image.SCALE_SMOOTH);
+			res_img1 = new ImageIcon(result);
+			imglabel1 = new JLabel(res_img1);
+			rating1.setBounds(65,7,55 , 15);
+			text1.setBounds(65, 350, 420, 120);
+			imglabel1.setBounds(65, 25, 550, 350);
+			text1.setText("<html><body>스튜디오 타입 디자인<br>스튜디오 타입 객실에 블루 패턴의 카페트와 화이트 패브릭 소파가 세련된 느낌을<br>"
+					+ "자아내며, 휴식을 취할 수 있는 다이닝 테이블과 체어를 완비한 제주신라의 객실로<br>"
+					+ "5~8층에 위치해 제주의 멋진 풍광을 감상 할 수 있는 색실입니다.");
+			center_content.add(rating1);
+			center_content.add(imglabel1);
+			center_content.add(text1);
+			center_content.add(room_select);
+			center_content.repaint();
+		}else if(room_select.getSelectedIndex() == 3){
+			center_content.removeAll();
+			rating1.setText("스위트");
+			img1 = new ImageIcon("img//호텔04(스위트).png");
+			resizeimage = img1.getImage();
+			result = resizeimage.getScaledInstance(550, 350, Image.SCALE_SMOOTH);
+			res_img1 = new ImageIcon(result);
+			imglabel1 = new JLabel(res_img1);
+			text1.setText("<html><body>편안함 느낌의 휴양 리조트 인테리어<br>코너 스위트는 본관 1층에서 6층까지 건물 윙의 가장 끝 부분에 위치하여 낮은 층에서는<br>"
+					+ "탁 트인 정원 전망을, 높은 층에서는 제주의 푸른 바다의 한라산의 기품을 함께 즐길 수<br>"
+					+ "있습니다.<br>"
+					+ "침실과 거실, 욕실이 분리되어 있으며 침실과 거실 사이는 루버도어로 장식이 되어 있습<br>"
+					+ "니다. 벽지, 커페트 등에 전체적으로 화이트, 제이드 그린, 블루 톤을 사용했으며, 캐노피<br>"
+					+ "침대와 클래식한 화장대로 스위트의 우아한 느낌을 더욱 살렸습니다."
+					+ "<br>침대옆에는 스툴이 있는 라탄 윙 체어를 놓아 리조트의 편안한 느낌을 연추 하였습니다.</body></html>");
+			rating1.setBounds(65,7,55 , 15);
+			text1.setBounds(65, 360, 420, 120);
+			imglabel1.setBounds(65, 25, 550, 350);
+			center_content.add(rating1);
+			center_content.add(imglabel1);
+			center_content.add(text1);
+			center_content.add(room_select);
+			center_content.repaint();
+		
+		}
+	}
 	Panel02(){
 		
 		this.setLayout(new BorderLayout());
 		north = new JPanel(new BorderLayout());
 		north_east = new JPanel(new FlowLayout());
 		
+		room_select = new JComboBox<String>(room);
+		room_select.addActionListener(this);
 		login = new JButton("로그인");
 		
 		north_east.add(login);
@@ -43,75 +121,19 @@ public class Panel02 extends JPanel{
 		center_content = new JPanel();
 		center_content.setLayout(null);
 		
-		rating1 = new JLabel("스텐다드");
-		img1 = new ImageIcon("img//호텔02(스텐다드).png");
-		resizeimage = img1.getImage();
-		result = resizeimage.getScaledInstance(400, 200, Image.SCALE_SMOOTH);
-		res_img1 = new ImageIcon(result);
-		imglabel1 = new JLabel(res_img1);
-		text1 = new JLabel("<html><body>기본타입 룸<br>스튜디오 타입의 파스텔 톤 객실에 휴식을 취할 수 있는"
-				+ "테이블과 체어, <br>그리고 라탄 타입의 아늑한 체어를 완비한 OO의 가장 기본적인 룸입니다.</body></html>");
-		
-		rating2 = new JLabel("프리미엄");
-		img2 = new ImageIcon("img//호텔03(프리미엄).png");
-		resizeimage = img2.getImage();
-		result = resizeimage.getScaledInstance(400, 200, Image.SCALE_SMOOTH);
-		res_img2 = new ImageIcon(result);
-		imglabel2 = new JLabel(res_img2);
-		text2 = new JLabel("<html><body>스튜디오 타입 디자인<br>스튜디오 타입 객실에 블루 패턴의 카페트와 화이트 패브릭 소파가 세련된 느낌을<br>"
-				+ "자아내며, 휴식을 취할 수 있는 다이닝 테이블과 체어를 완비한 제주신라의 객실로<br>"
-				+ "5~8층에 위치해 제주의 멋진 풍광을 감상 할 수 있는 색실입니다.");
-		
-		rating3 = new JLabel("스위트");
-		img3 = new ImageIcon("img//호텔04(스위트).png");
-		resizeimage = img3.getImage();
-		result = resizeimage.getScaledInstance(400, 200, Image.SCALE_SMOOTH);
-		res_img3 = new ImageIcon(result);
-		imglabel3 = new JLabel(res_img3);
-		text3 = new JLabel("<html><body>편안함 느낌의 휴양 리조트 인테리어<br>코너 스위트는 본관 1층에서 6층까지 건물 윙의 가장 끝 부분에 위치하여 낮은 층에서는<br>"
-				+ "탁 트인 정원 전망을, 높은 층에서는 제주의 푸른 바다의 한라산의 기품을 함께 즐길 수<br>"
-				+ "있습니다.<br>"
-				+ "침실과 거실, 욕실이 분리되어 있으며 침실과 거실 사이는 루버도어로 장식이 되어 있습<br>"
-				+ "니다. 벽지, 커페트 등에 전체적으로 화이트, 제이드 그린, 블루 톤을 사용했으며, 캐노피<br>"
-				+ "침대와 클래식한 화장대로 스위트의 우아한 느낌을 더욱 살렸습니다."
-				+ "<br>침대옆에는 스툴이 있는 라탄 윙 체어를 놓아 리조트의 편안한 느낌을 연추 하였습니다.</body></html>");
+		rating1 = new JLabel();
+		img1 = new ImageIcon();
+		imglabel1 = new JLabel();
+		text1 = new JLabel();
+
+
+		room_select.setBounds(650, 7, 150, 25);
 		
 		
-		
-		rating1.setBounds(65,7,55 , 15);
-		imglabel1.setBounds(65, 25, 400, 200);
-		text1.setBounds(65, 190, 420, 120);
-		
-		rating2.setBounds(65,320,55, 15);
-		imglabel2.setBounds(65, 340, 400, 200);
-		text2.setBounds(65, 530, 420, 150);
-		
-		rating3.setBounds(65,690,55 , 15);
-		imglabel3.setBounds(65, 710, 400, 200);
-		text3.setBounds(65, 860, 420, 350);
-		
-		center_content.add(rating1);
-		center_content.add(imglabel1);
-		center.setAlignmentX(imglabel1.LEFT_ALIGNMENT);
-		center_content.add(text1);
-		
-		//center_content.add(content_1);
-		
-		center_content.add(rating2);
-		center_content.add(imglabel2);
-		center.setAlignmentX(imglabel2.LEFT_ALIGNMENT);
-		center_content.add(text2);
-		
-		center_content.add(rating3);
-		center_content.add(imglabel3);
-		center.setAlignmentX(imglabel3.LEFT_ALIGNMENT);
-		center_content.add(text3);
-		
+		center_content.add(room_select);
 		
 		center.add(center_content);
-
-		
-		
+		center_content.setBackground(new Color(0xb6e7f0));
 		this.add("Center",center);
 		this.add("North", north);
 		
--- team_project/src/cho_jeahyun/Panel07.java
+++ team_project/src/cho_jeahyun/Panel07.java
@@ -1,21 +1,33 @@
 package cho_jeahyun;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.EventObject;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.AbstractAction;
+import javax.swing.AbstractCellEditor;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.CellEditorListener;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
 
 //     예약확인
-class Reservation_Info_Model extends AbstractTableModel {
+class Reservation_Info_Model extends AbstractTableModel{
+	
+
 	private Object[][] tableData;
 	int cols, rows;
+	private JButton button = new JButton("취소");
 	private String[] columnName = {"예약번호","예약날짜","예약자 명","방 번호","체크인/체크아웃","가격"," "};
 	private List list;
 	
@@ -30,6 +42,7 @@ class Reservation_Info_Model extends AbstractTableModel {
 	}
 
 	void setData() {
+		
 		Iterator it = list.iterator();
 		rows = list.size();
 		cols = columnName.length;
@@ -43,7 +56,7 @@ class Reservation_Info_Model extends AbstractTableModel {
 			tableData[r][3] = res.getRoomid();
 			tableData[r][4] = res.getCheck_in_d()+res.getCheck_out_d();
 			tableData[r][5] = res.getTotal_price();
-			
+//			tableData[r][6] = button;
 			r++;
 		}
 	}
@@ -65,6 +78,8 @@ class Reservation_Info_Model extends AbstractTableModel {
 		return tableData[rowIndex][columnIndex];
 	}
 
+	
+
 }
 public class Panel07 extends JPanel implements ActionListener{
 	@Override
@@ -73,13 +88,19 @@ public class Panel07 extends JPanel implements ActionListener{
 		JButton button = (JButton)e.getSource();
 		if(button == select) {
 			table.setModel(new Reservation_Info_Model());
+			table.getColumnModel().getColumn(6).setCellRenderer(new TableCell());
+			table.getColumnModel().getColumn(6).setCellEditor(new TableCell());
 		}
+//		if(button == cancel) {
+//			System.out.println("sdasd");
+//		}
 	}
 
 	private JPanel tabPanel, north_right;
 	private JButton select;
 	private JTable table;
 	private JScrollPane tablescroll;
+	private JButton cancel;
 	
 	
 	Panel07(){
@@ -94,12 +115,54 @@ public class Panel07 extends JPanel implements ActionListener{
 		
 		
 		table = new JTable();
-		tablescroll = new JScrollPane(table);
 		
+		tablescroll = new JScrollPane(table);
+
 		
 		this.add("North", north_right);
 		this.add("Center",tablescroll);
 		
 		this.setVisible(true);
+	}
+	public void test(){
+		System.out.println("sadasdawq");
+	}
+	
+	class TableCell extends AbstractCellEditor implements TableCellEditor, TableCellRenderer{
+
+		
+
+		public TableCell() {
+			// TODO Auto-generated constructor stub
+			cancel = new JButton("취소");
+//			cancel.setOpaque(true);
+//			cancel.addActionListener(e -> {
+//				System.out.println(table.getValueAt(table.getSelectedRow(), table.getSelectedColumn()));
+//			});
+			cancel.addActionListener(e ->{
+				test();
+			});
+		
+		}
+		@Override
+		public Object getCellEditorValue() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+			// TODO Auto-generated method stub
+			return cancel;
+		}
+
+		@Override
+		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
+				int column) {
+			// TODO Auto-generated method stub
+			return cancel;
+		}
+		
 	}
 }
