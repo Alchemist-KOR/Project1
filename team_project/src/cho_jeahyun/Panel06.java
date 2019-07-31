334645c494ed41dcf26cadaebd79fae4617da3b5
user <user@user-PC>
2019-07-31 16:27:33 +0900

    이쪽ㅇㅣ진짜

--- team_project/src/cho_jeahyun/Panel06.java
+++ team_project/src/cho_jeahyun/Panel06.java
@@ -1,9 +1,22 @@
 package cho_jeahyun;
 
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Panel06 extends JPanel{
-	Panel06(){
-		
+	private JPanel north;
+	private JLabel rogo;
+	private Image rogo1, rogo2;
+	public Panel06(){
+		north = new JPanel();
+		ImageIcon icon_rogo = new ImageIcon("src//cyc//rogo.jpg");
+		rogo1 = icon_rogo.getImage();
+		rogo2 = rogo1.getScaledInstance(100, 50, Image.SCALE_SMOOTH);
+		ImageIcon icon_rogo2 = new ImageIcon(rogo2);
+		rogo = new JLabel(icon_rogo2);
+		north.add("Center",rogo);
 	}
 }
