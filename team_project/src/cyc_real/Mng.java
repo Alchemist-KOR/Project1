4beba44a0c38f0e17b2d32455b5c409424cc49dc
user <user@user-PC>
2019-08-02 15:19:31 +0900

    rhxhd

---
+++ team_project/src/cyc_real/Mng.java
@@ -0,0 +1,33 @@
+package cyc_real;
+
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class Mng extends JFrame {
+//	private CardLayout card;
+	private JPanel sales, reservation;
+	public Mng_Customer customer = null;
+	public Mng(){
+		this.setVisible(true);
+		this.setSize(850, 700);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		customer = new Mng_Customer();
+		sales = new JPanel();
+		reservation = new Mng_Reservation();
+		JTabbedPane tab = new JTabbedPane();
+		tab.addTab("绊按包府", customer);
+		tab.addTab("概免包府", sales);
+		tab.addTab("抗距包府", reservation);
+		this.add("Center",tab);
+
+	}
+	public static void main(String[] args) {
+		
+		new Mng();
+	}
+
+}
