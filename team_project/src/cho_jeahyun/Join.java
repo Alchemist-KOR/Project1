7a9c8b4efa9b32e44cc14d371f2c93546a30e9ff
ddung <ddung@MSI-GS73VR>
2019-08-02 17:58:27 +0900

    0802 마무리

--- team_project/src/cho_jeahyun/Join.java
+++ team_project/src/cho_jeahyun/Join.java
@@ -1,5 +1,6 @@
 package cho_jeahyun;
 
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -62,20 +63,22 @@ public class Join extends JFrame implements ActionListener{
 			}else {
 				JOptionPane.showMessageDialog(this, "등록 중 문제가 발생했습니다.");
 			}
+		}else if(obj == calendar_btn) {
+			new CalendarBySwing(this);
 		}
 	}
 	JLabel name_label,id_label,pwd_label,
 			birth_label,gender_label,
 			email_label,addr_label,
-			phoneNum_label,information_label;
+			phoneNum_label,information_label,temp;
 	JTextField name_txt,id_txt,birth_txt,
 				email_txt,addr_txt,phoneNum_txt;
 	JPasswordField pwd_txt;
 	JRadioButton man,woman;
 	ButtonGroup group;
 	JCheckBox info_check;
-	JPanel gender_panel,center,south;
-	JButton join_btn;
+	JPanel gender_panel,center,south, calendar_panel;
+	JButton join_btn ,calendar_btn;
 	
 	public Join(){
 		name_label=new JLabel("이름");
@@ -87,14 +90,18 @@ public class Join extends JFrame implements ActionListener{
 		addr_label=new JLabel("주소");
 		phoneNum_label=new JLabel("전화번호");
 		information_label=new JLabel("제 3자 정보 제공 동의");
+		temp = new JLabel("");
 		
 		name_txt=new JTextField(15);
 		id_txt=new JTextField(15);
 		pwd_txt=new JPasswordField(15);
-		birth_txt=new JTextField("예시)2000/01/01",15);
+		birth_txt=new JTextField("예시)2000/01/01",11);
+		calendar_btn = new JButton("달력");
 		email_txt=new JTextField(15);
 		addr_txt=new JTextField(15);
 		phoneNum_txt=new JTextField("예시)01012345678",15);
+		
+		calendar_btn.addActionListener(this);
 		
 		man=new JRadioButton("남자");
 		woman=new JRadioButton("여자");
@@ -109,17 +116,21 @@ public class Join extends JFrame implements ActionListener{
 		gender_panel=new JPanel();
 		gender_panel.add(man);gender_panel.add(woman);
 		
-		center=new JPanel(new GridLayout(9,2));
+		center=new JPanel(new GridLayout(10,2));
+		calendar_panel = new JPanel(new FlowLayout());
 		
 		center.add(name_label);center.add(name_txt);
 		center.add(id_label);center.add(id_txt);
 		center.add(pwd_label);center.add(pwd_txt);
-		center.add(birth_label);center.add(birth_txt);
+		calendar_panel.add(birth_txt);calendar_panel.add(calendar_btn);
+		center.add(birth_label);center.add(calendar_panel);
+//		center.add(temp); center.add(temp);
 		center.add(gender_label);center.add(gender_panel);
 		center.add(email_label);center.add(email_txt);
 		center.add(addr_label);center.add(addr_txt);
 		center.add(phoneNum_label);center.add(phoneNum_txt);
 		center.add(information_label);center.add(info_check);
+		setLabel();
 		
 		south=new JPanel();
 		south.add(join_btn);
@@ -128,10 +139,21 @@ public class Join extends JFrame implements ActionListener{
 		this.add("South",south);
 		
 		this.setTitle("회원가입");
-		this.setSize(400,500);
+		this.setSize(450,500);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+	void setLabel() {
+		name_label.setHorizontalAlignment(JLabel.CENTER);
+		id_label.setHorizontalAlignment(JLabel.CENTER);
+		pwd_label.setHorizontalAlignment(JLabel.CENTER);
+		birth_label.setHorizontalAlignment(JLabel.CENTER);
+		gender_label.setHorizontalAlignment(JLabel.CENTER);
+		email_label.setHorizontalAlignment(JLabel.CENTER);
+		addr_label.setHorizontalAlignment(JLabel.CENTER);
+		phoneNum_label.setHorizontalAlignment(JLabel.CENTER);
+		information_label.setHorizontalAlignment(JLabel.CENTER);
 	}
 //	public static void main(String[] args) {
 //		new Join();
